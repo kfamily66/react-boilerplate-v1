5cfe8633ee1712980e271cd9b5f07b686e0556ba
unknown <kfamily66@yandex.ru>
2018-06-08 18:37:22 +0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css";
+import "./styles/style.scss";
+
+ReactDOM.render(
+  <p> This is a react-boilerplate</p>,
+  document.getElementById("app")
+);
